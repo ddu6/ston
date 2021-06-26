014e7bd81f037f14afe3d271a4a95e78cc3d767e
ddu <ddu6@protonmail.com>
2021-06-26 16:50:26 +0800

    update

---
+++ dist/mod.d.ts
@@ -0,0 +1,8 @@
+export declare type STONObject = {
+    [key: string]: (STONObject | STONArray | string | number | boolean);
+};
+export declare type STONArray = (STONObject | STONArray | string | number | boolean)[];
+export declare function parse(string: string): STONObject | STONArray | string | number | boolean | undefined;
+declare type BeautifyTarget = 'array' | 'object' | 'all' | 'none' | 'arrayInObject' | 'arrayInObjectAndThis';
+export declare function stringify(ston: STONObject | STONArray | string | number | boolean, beautify?: BeautifyTarget, level?: number): string;
+export {};
