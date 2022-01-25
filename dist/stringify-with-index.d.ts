4ee8856a7794675c0c809d2664c34eb500f1bcd4
DDU <ddu6@protonmail.com>
2022-01-26 03:28:40 +0800

    update

--- dist/stringify-with-index.d.ts
+++
@@ -1,3 +0,0 @@
-import type { STONWithIndexValue } from './parse-with-index';
-import type { BeautifyOptions } from './stringify';
-export declare function stringifyWithComment(ston: STONWithIndexValue | undefined, beautifyOptions?: BeautifyOptions): string;
